6b8707025da39e4633b517dcb645d167499bdde5
CoolCoder64 <GitHub@otmfamily.com>
2024-05-16 14:44:50 +1000

    Fixed log file bug

--- middleware/logger.js
+++ middleware/logger.js
@@ -4,17 +4,15 @@ const logger = winston.createLogger({
     level: 'info',
     format: winston.format.combine(
         winston.format.timestamp(),
-        winston.format.printf(({ level, message, timestamp }) => {
-            const color = level === 'error' ? 'red' : level === 'warn' ? 'yellow' : 'white';
-            return `\x1b[1m\x1b[${color}m${timestamp} [${level}]: ${message}\x1b[0m`;
-        })
+        winston.format.json()
     ),
     transports: [
         new winston.transports.Console({
             format: winston.format.combine(
                 winston.format.colorize(),
                 winston.format.printf(({ level, message, timestamp }) => {
-                    return `${timestamp} [${level}]: ${message}`;
+                    const color = level === 'error' ? 'red' : level === 'warn' ? 'yellow' : 'white';
+                    return `\x1b[1m\x1b[${color}m${timestamp} [${level}]: ${message}\x1b[0m`;
                 })
             ),
             handleExceptions: true,
@@ -24,8 +22,8 @@ const logger = winston.createLogger({
             timestamp: true,
             stderrLevels: ['error', 'warn']
         }),
-        new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'logs/combined.log' })
+        new winston.transports.File({ filename: 'logs/error.log', level: 'error', format: winston.format.json() }),
+        new winston.transports.File({ filename: 'logs/combined.log', format: winston.format.json() })
     ]
 });
 
